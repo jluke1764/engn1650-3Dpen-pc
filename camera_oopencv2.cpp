e027d62d4f6a16bb192e9f2f26ae05b061c604f9
VivianChan1998 <b05901179@ntu.edu.tw>
2022-12-03 19:38:29 -0500

    init

---
+++ camera_oopencv2.cpp
@@ -0,0 +1,24 @@
+#include <opencv2\opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main() {
+
+    Mat image;
+    namedWindow("Display window");
+
+    VideoCapture cap(0);
+
+    if (!cap.isOpened()) {
+        cout << "cannot open camera";
+    }
+
+    while (true) {
+        cap >> image;
+        imshow("test", image);
+        waitKey(25);
+    }
+
+    return 0;
+}
---
+++ main_kens_code.cpp
@@ -0,0 +1,64 @@
+#include <ctime>
+#include <math.h>
+
+typedef struct Point3D {
+    float x;
+    float y;
+    float z;
+} Point3D;
+static Point3D ihaf;
+
+
+int main()
+{
+   
+}
+
+void getperpvec (Point3D n, Point3D a, Point3D b)
+{
+   float f;
+   if (fabs(n.x) < fabs(n.y))
+   {
+      f = 1/sqrt(n.y*n.y + n.z*n.z);
+      a.x = 0;
+      a.y = n.z*f;
+      a.z =-n.y*f;
+   }
+   else
+   {
+      f = 1/sqrt(n.x*n.x + n.z*n.z);
+      a.x = -n.z*f;
+      a.y =     0;
+      a.z = n.x*f;
+   }
+   b.x = n.y*a.z - n.z*a.y;
+   b.y = n.z*a.x - n.x*a.z;
+   b.z = n.x*a.y - n.y*a.x;
+   f = 1/sqrt(b.x*b.x + b.y*b.y + b.z*b.z);
+   b.x *= f;
+   b.y *= f;
+   b.z *= f;
+}
+
+void ang2mat (float hang, float vang, float mat[9])
+{
+   mat[6] = cos(vang)*sin(hang); mat[0] = cos(hang);
+   mat[7] = sin(vang);           mat[1] = 0;
+   mat[8] = cos(vang)*cos(hang); mat[2] =-sin(hang);
+   mat[3] = mat[7]*mat[2] - mat[8]*mat[1];
+   mat[4] = mat[8]*mat[0] - mat[6]*mat[2];
+   mat[5] = mat[6]*mat[1] - mat[7]*mat[0];
+}
+//-----------------------------------------------------------------------------
+void sph2conic (float cx, float cy, float cz, float  &a, float &b, float &c, float &d, float &e,float &f)
+{
+   float cx2 = cx*cx; float cy2 = cy*cy; float cz2 = cz*cz;
+   a = 1-cy2-cz2; e = cy*cz*2;
+   c = 1-cx2-cz2; d = cx*cz*2;
+   f = 1-cx2-cy2; b = cx*cy*2;
+   
+      //Xform conic from {-1..1} to screen coord: x = x*ihaf.z+ihaf.x; y = y*ihaf.z+ihaf.y;
+   e = e*ihaf.z - c*ihaf.y*2 - b*ihaf.x;
+   d = d*ihaf.z - a*ihaf.x*2 - b*ihaf.y;
+   f = f*ihaf.z*ihaf.z - a*ihaf.x*ihaf.x - b*ihaf.x*ihaf.y - c*ihaf.y*ihaf.y - d*ihaf.x - e*ihaf.y;
+}
