515579ddd7ab4cb9e9f4680e468a73a238009a59
VivianChan1998 <b05901179@ntu.edu.tw>
2023-05-08 00:06:12 -0400

    minor bugfix

--- application/3d-pen/Assets/Scripts/Connect.cs
+++ application/3d-pen/Assets/Scripts/Connect.cs
@@ -64,13 +64,15 @@ public class Connect : MonoBehaviour
                         string serverMessage = Encoding.ASCII.GetString(incommingData);
                         //Debug.Log("received: " + serverMessage);
                         string[] datas = serverMessage.Split(',');
-                        pos.x = (datas[0]=="1"? 1 : -1) * float.Parse(datas[1]) *2;
-                        pos.y = (datas[2] == "1" ? 1 : -1) * float.Parse(datas[3])*2;
-                        pos.z = (datas[4] == "1" ? 1 : -1) * float.Parse(datas[5])*2;
-                        Debug.Log(pos.x);
-                        Debug.Log(pos.y);
-                        Debug.Log(pos.z);
-
+                        if(datas.Length >= 6)
+                        {
+                            pos.x = (datas[0] == "1" ? 1 : -1) * float.Parse(datas[1]) * 2;
+                            pos.y = (datas[2] == "1" ? 1 : -1) * float.Parse(datas[3]) * 2;
+                            pos.z = (datas[4] == "1" ? 1 : -1) * float.Parse(datas[5]) * 2;
+                            //Debug.Log(pos.x);
+                            //Debug.Log(pos.y);
+                            //Debug.Log(pos.z);
+                        }
                     }
                 }
             }
@@ -95,7 +97,6 @@ public class Connect : MonoBehaviour
 
     void Update()
     {
-        Debug.Log("here");
         cursor.transform.position = pos;
     }
 }
